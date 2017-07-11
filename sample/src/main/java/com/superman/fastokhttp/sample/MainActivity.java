3d696e9a00bebfcb9745c3fe9d1365d30081339c
Super <986386695@qq.com>
2017-07-11 22:22:05 +0800

    提交代码

---
+++ sample/src/main/java/com/superman/fastokhttp/sample/MainActivity.java
@@ -0,0 +1,22 @@
+package com.superman.fastokhttp.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.test).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplication(),"hello",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
