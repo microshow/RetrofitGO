b1c23696b5ab9e7a49978df9e929680c1f2c8ccc
Super <986386695@qq.com>
2017-07-11 22:30:32 +0800

    修改代码

--- sample/src/main/java/com/superman/fastokhttp/sample/MainActivity.java
+++ sample/src/main/java/com/superman/fastokhttp/sample/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.test).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplication(),"hello",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplication(),"helloworld",Toast.LENGTH_SHORT).show();
             }
         });
     }
